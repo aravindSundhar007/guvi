3b50b92335959c99bc9f28f6f021a92823ad24b2
aravindSundhar007 <51356025+aravindSundhar007@users.noreply.github.com>
2019-06-12 23:14:16 +0530

    Update 3.py

--- code/3.py
+++ code/3.py
@@ -1,6 +1,6 @@
 ch=str(input())
 if (ch=="a" or ch=="e" or ch=="o" or ch=="i" or ch=="u"):
-    print("Vowels")
+    print("Vowel")
 elif(ch=="b" or ch=="c" or ch=="d" or ch=="f" or ch=="g"or ch=="h" or ch=="j" or ch=="k" or ch=="l" or ch=="m" or ch=="n" or ch=="p" or ch=="q" or ch=="r" or ch=="s"or ch=="t" or ch=="v" or ch=="w" or ch=="x" or ch=="y"or ch=="z"):
     print("Consonant")
 else:
