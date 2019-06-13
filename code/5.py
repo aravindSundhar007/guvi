d17d969c6e64d89ad1559e80669bf4e27f89bf1e
aravindSundhar007 <51356025+aravindSundhar007@users.noreply.github.com>
2019-06-13 18:31:23 +0530

    Update 5.py

--- code/5.py
+++ code/5.py
@@ -1,9 +1,9 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
-if (num1>=num2) and (num1>=num3):
-    print(num1)
-elif(num2>=num1) and (num2>=num3):
-    print(num2)
+nos1=int(input())
+nos2=int(input())
+nos3=int(input())
+if (nos1>=nos2) and (nos1>=nos3):
+    print(nos1)
+elif(nos2>=nos1) and (nos2>=nos3):
+    print(nos2)
 else:
-    print(num3)
+    print(nos3)
